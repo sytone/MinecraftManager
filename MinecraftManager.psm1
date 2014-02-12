b05b28d90625a4f79f7337a35e299d4c05b8a912
sytone <github@sytone.com>
2014-02-12 07:45:33 -0800

    Create MinecraftManager.psm1

---
+++ MinecraftManager.psm1
@@ -0,0 +1,31 @@
+##############
+#
+# Minecraft Manager
+#
+##############
+#
+# 0.1 - Inital work.
+#
+#
+#
+
+function New-MinecraftServer {
+}
+
+function Stop-MinecraftServer {
+}
+
+function Start-MinecraftServer {
+}
+
+function Restart-MinecraftServer {
+}
+
+function Remove-MinecraftServer {
+}
+
+function Install-MinecraftServer {
+}
+
+function Grant-MCServerAccess {
+}
